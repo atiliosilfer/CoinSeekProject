8d028836220d0f8fe6ff7ddcb6455e9494ee8e94
Atílio Silva Ferreira <42559266+atiliosilfer@users.noreply.github.com>
2019-11-26 16:57:16 -0300

    Update Cotacao.java

--- app/src/main/java/com/example/trabalho_final/Cotacao.java
+++ app/src/main/java/com/example/trabalho_final/Cotacao.java
@@ -1,5 +1,47 @@
 package com.example.trabalho_final;
 
+    /** Exemplo do Retorno
+         {
+         "success": true,
+         "timestamp": 1574792046,
+         "base": "EUR",
+         "date": "2019-11-26",
+         "rates": {
+         "ARS": 66.045879,
+         "AUD": 1.623833,
+         "BOB": 7.620355,
+         "BRL": 4.706576,
+         "BTC": 0.000156,
+         "CAD": 1.463267,
+         "CHF": 1.099071,
+         "CLP": 881.021272,
+         "CNY": 7.750445,
+         "COP": 3778.394926,
+         "EGP": 17.763592,
+         "EUR": 1,
+         "GBP": 0.856972,
+         "HKD": 8.626535,
+         "INR": 78.785434,
+         "JPY": 120.220301,
+         "KPW": 991.793437,
+         "KRW": 1293.173177,
+         "MXN": 21.51953,
+         "NZD": 1.715257,
+         "PEN": 3.725085,
+         "PYG": 7099.721291,
+         "RUB": 70.49647,
+         "SEK": 10.567426,
+         "SGD": 1.503306,
+         "USD": 1.101933,
+         "UYU": 41.635903,
+         "VEF": 11.005558,
+         "XAG": 0.064535,
+         "XAU": 0.000755,
+         "XCD": 2.978028
+         }
+         }
+     */
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -20,6 +62,30 @@ public class Cotacao {
     @SerializedName("rates")
     @Expose
     private Rates rates;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Cotacao() {
+    }
+
+    /**
+     *
+     * @param date
+     * @param success
+     * @param rates
+     * @param timestamp
+     * @param base
+     */
+    public Cotacao(Boolean success, Integer timestamp, String base, String date, Rates rates) {
+        super();
+        this.success = success;
+        this.timestamp = timestamp;
+        this.base = base;
+        this.date = date;
+        this.rates = rates;
+    }
 
     public Boolean getSuccess() {
         return success;
@@ -62,4 +128,3 @@ public class Cotacao {
     }
 
 }
-
